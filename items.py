5a0bcdbac2bd24271164b06e6aeca513e130b05c
JCMerlino <jorgecorreamerlino@gmail.com>
2017-10-16 10:56:48 +0100

    The bases of the game

---
+++ items.py
@@ -0,0 +1,63 @@
+item_id = {
+    "id": "id",
+
+    "name": "id card",
+
+    "mass": 0.1,
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?..."""
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "name": "laptop",
+
+    "mass": 1.7,
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!"
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "money",
+
+    "mass": 1,
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees."
+}
+
+item_biscuits = {
+    "id": "biscuits",
+
+    "name": "a pack of biscuits",
+
+    "mass": 0.7,
+
+    "description": "A pack of biscuits."
+}
+
+item_pen = {
+    "id": "pen",
+
+    "name": "a pen",
+
+    "mass": 0.3,
+
+    "description": "A basic ballpoint pen."
+}
+
+item_handbook = {
+    "id": "handbook",
+
+    "name": "a student handbook",
+
+    "mass": 1.2,
+
+    "description": "This student handbook explains everything. Seriously."
+}
---
+++ player.py
@@ -0,0 +1,7 @@
+from items import *
+from map import rooms
+
+inventory = [item_id]
+
+# Start game at the reception
+current_room = rooms["Reception"]
