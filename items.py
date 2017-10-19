ae30a36779d1c74e7f1c8784b6fb233ab7f22938
Spooky-Jiangshi <owen.choy99@googlemail.com>
2017-10-19 16:11:03 +0100

    updated items list

--- items.py
+++ items.py
@@ -1,63 +1,172 @@
-item_id = {
-    "id": "id",
+item_usb = {
+    "id": "USB",
 
-    "name": "id card",
+    "name": "Stephen's USB",
+
+    "mass": 1.0,
+
+    "description":
+    """Stephen gave me this USB with his so called "Mainframe Fix" on it. 
+The last time he handed me one of these he left extremely illegal pornography that really made me question his functioning capacity as a human being. 
+He told me to plug this into the mainframe and log on. Of course he didn't give me the login details so I'll have to go find out some. 
+I'm sure it would have made my life a lot easier if he gave me details but I assume he wants me to use someone else's account so the blame goes on them if it screws up. 
+Pretty smart. I think I'll screw up Bob's account."""
+}
+
+item_mainframe_key = {
+    "id": "mainframe_key",
+
+    "name": "mainframe key",
+
+    "mass": 0.5,
+
+    "description":
+    """Well we just won shitest security system award of the year. Seriously this is a physical key that unlocks the door to the mainframe. I mean, someone would still have to hack the mainframe but man why just a single key?
+Anyone could just pick that lock. Wait... I knew it! I didn't search up furry porn when I was blackout drunk it was Stephen who picked the lock and changed the mainframe entries. Such a dick. Come to think of it now it is pretty funny. 
+Maybe I should get him to do it on Bob."""
+}
+
+item_login_details = {
+    "id": "login",
+
+    "name": "login details",
+
+    "mass": 0.2,
+
+    "description": 
+    """Trust Bob to leave his login details on a scrap peice of paper. Too easy. Login:Bob Password:Bruceisacunt 
+Considering how much I do for this service I think I deserve a little respect."""
+}
+
+item_ink = {
+    "id": "ink",
+
+    "name": "ink cartridge",
+
+    "mass": 0.5,
+
+    "description": 
+    """Am I the secretary as well? All Jenifer needs to do is replace the ink cardtridge when the printer runs out of ink but no let the field agent do it because why the hell not!
+Come to think of I should file a complaint because I seem to be the only one who does any work around here. Everyone else just stands around in their rooms not even moving."""
+}
+
+item_vault_log = {
+    "id": "vault_log",
+
+    "name": "vault logs",
+
+    "mass": 0.8,
+
+    "description": 
+    """So printed out on this piece of paper is every item in the vault. Apparently Stephen can see if there is something fishy based on the times and entries of the information. 
+Sounds like a very boring and tedious task. Thank god he didn't ask me to do it"""
+}
+
+item_vault_key = {
+    "id": "vault_key",
+
+    "name": "vault key",
+
+    "mass": 0.5,
+
+    "description": 
+    """Urgh its still wet with toilet water. Makes me want to hurl. Why the hell would anyone put it here? Or more worringly did someone eat it?
+Anyway maybe our security system should just not exist at all. At least this key only unlocks the outer door. 
+Of course this means I also need the Vault Password as well."""
+}
+
+item_vault_pass = {
+    "id": "vault_pass",
+
+    "name": "vault password",
 
     "mass": 0.1,
 
-    "description":
-    """You new shiny student ID card. Expires 1 June 2017.
-You wonder why they have printed a suicide hotline number on it?..."""
+    "description": 
+    """Well at least the actual password for the vault is decent. 10 digit number which is like a lot of combinations. 1544973268 is the password
+I'm glad this password is decent. I put my trust in this password to gaurd my secret stash. I don't know why people think I have a problem when I clearly don't"""
 }
 
-item_laptop = {
-    "id": "laptop",
+item_mugs = {
+    "id": "mugs",
 
-    "name": "laptop",
+    "name": "mugs",
 
-    "mass": 1.7,
+    "mass": 0.8,
 
-    "description":
-    "It has seen better days. At least it has a WiFi card!"
+    "description": 
+    """These mugs come from the break room. I have no idea why Stephen needs these mugs. They aren't high tec or anything. Maybe he needs the ceramics. I hope Debra and Jenifer don't mind me taking their ones. 
+No way I'm letting my mug being destroyed. I won it for best hairstyle at a primary school faire"""
 }
 
-item_money = {
-    "id": "money",
+item_power_lead = {
+    "id": "lead",
 
-    "name": "money",
+    "name": "power lead",
 
-    "mass": 1,
+    "mass": 0.8,
 
-    "description":
-    "This wad of cash is barely enough to pay your tuition fees."
+    "description": 
+    """You'd think that the science lab would have these just lying about but apparently not. I think Krieger is just being lazy and making me do his errands.
+The computer I took this from did say "DO NOT POWER OFF" but I think it was being melodramatic. I'm pretty sure things will be fine."""
 }
 
-item_biscuits = {
-    "id": "biscuits",
+item_heat_plate = {
+    "id": "heat_plate",
 
-    "name": "a pack of biscuits",
+    "name": "heat plate",
+
+    "mass": 1.0,
+
+    "description": 
+    """Litterally in the next room. Couldn't he just get off his lazy ass and go get it himself? This heat plate looks heavily used. But then again Stephen does like sloppy seconds"""
+}
+
+item_documents = {
+    "id": "documents",
+
+    "name": "documents",
+
+    "mass": 0.5,
+
+    "description": 
+    """Well this is unexpected. These are copies of all the agent files here. Our physical reports, mental reports, health reports. Jeez these documents have everything. 
+Anyone who gets hold of these will know everything about every field agent. I wish I'd found this sooner, these health and physical reports on female agents are rather interesting..."""
+}
+
+item_gun = {
+    "id": "gun",
+
+    "name": "gun",
+
+    "mass": 1.5,
+
+    "description": 
+    """This was found hidden in the meeting room. Considering there are no agents in the field currently and that no one should have access to a gun at all. 
+Perhaps I should dust it for prints or whatever forensic people do. """
+}
+
+item_camera = {
+    "id": "camera",
+
+    "name": "camera",
 
     "mass": 0.7,
 
-    "description": "A pack of biscuits."
+    "description": 
+    """They say profesional work conduct is essential for a safe and effective working environment but someone obviously put this here to catch people getting a little too close to each other. 
+I wonder what sort of creep wants to record their co-workers have unprofessional sex. I better find out who put this camera in the meeting room. Actually maybe this camera was used to record important meetings by a mole or something.
+Either way I don't want them finding out what else goes on in the meeting room especially if it concerns me."""
 }
 
-item_pen = {
-    "id": "pen",
+item_recorder = {
+    "id": "recorder",
 
-    "name": "a pen",
+    "name": "recorder",
 
-    "mass": 0.3,
+    "mass": 0.5,
 
-    "description": "A basic ballpoint pen."
-}
-
-item_handbook = {
-    "id": "handbook",
-
-    "name": "a student handbook",
-
-    "mass": 1.2,
-
-    "description": "This student handbook explains everything. Seriously."
+    "description": 
+    """This was obviously put in Samantha's room to eavesdrop on her conversations. I wouldn't be suprised if it was one of us who put it there because she makes shady deals with other organisations that really screw us over. 
+Though this might be linked to the camera and about gathering important information. Wouldn't that be embarrassing... If the professional spies were being spied on. I should probably alert Debra about a possible double agent"""
 }
