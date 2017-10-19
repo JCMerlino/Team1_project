9034f68ea0ff0daa775f2bc25e613f34d50613f8
ConnorCf <32736687+ConnorCf@users.noreply.github.com>
2017-10-19 15:32:19 +0100

    Add files via upload

---
+++ characters.py
@@ -0,0 +1,83 @@
+from items import *
+from map import rooms
+
+# Start game at the Mallory
+current_room = rooms["Mallory"]
+
+
+
+Bruce = {
+
+    "name": "Bruce",
+
+    "description":
+    "Bruce is a world renound spy, with incredible skill and combat abilities.",
+
+	inventory = []
+
+    
+}
+
+Samantha = {
+
+    "name": "Samantha",
+
+    "description":
+    "Samantha is Bruce's boss, she is ruthless and wants the job done!",
+
+    inventory = []
+
+}
+
+Jenifer = {
+    "name": "Jenifer",
+
+    "description":
+    "Jenifer is Samantha's secretary, she is useless at her job",
+
+	inventory = []
+}
+
+Debra = {
+
+    "name": "Debra",
+
+    "description":
+    "Debra is head of Human Resources ",
+
+}
+
+Bob = {
+    "name": "Bob",
+
+    "name": "id card",
+
+    "description":
+    "Bob is the Document manager",
+
+	inventory = []
+
+}
+
+Stephen = {
+
+    "name": "Stephen",
+
+    "description":
+    "Stephen is the technician for the agency",
+
+	inventory = []
+
+}
+
+Alexa = {
+
+    "name": "Alexa",
+
+    "description":
+    "Alexa is a spy, she is very rude and harsh",
+
+	inventory = []
+    
+}
+
