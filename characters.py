b0b1097767facad357b6ebe3fd4b2b973d15b365
Owen <Spooky-Jiangshi@github.com>
2017-10-21 20:22:14 +0100

    Updated character descriptions

--- characters.py
+++ characters.py
@@ -5,7 +5,12 @@ Bruce = {
     "name": "Bruce",
 
     "description":
-    "Bruce is a world renound spy, with incredible skill and combat abilities.",
+    """Bruce is a world renound spy working for a secret organisation few know about.
+    Despite his high level clearance for top secret intelligence he remains extremely
+    immature and fails to take any matter seriously. Whilst it is a mystery to some 
+    how his integrity of success remains it is clear to those who know him. He is the 
+    best. He is immature. He fails to see the gravity of his actions. But he is the 
+    best at what he does and no one can replace him.""",
 
     "inventory": []
 
@@ -16,7 +21,16 @@ Samantha = {
     "name": "Samantha",
 
     "description":
-    "Samantha is Bruce's boss, she is ruthless and wants the job done!",
+    """Samantha is the head of the secret organisation which shall not be named.
+    She earned her position through years of undercover work and on more than one
+    occasion defending classified intelligence with her life. Whilst she is 
+    percieved as an old hag by the rest of the organisation she understands how 
+    important the organisation is to the safety and security of the nation. As a 
+    result she takes every action with upmost seriousness and is constantly 
+    reminding others to do so as well. To her faliure Bruce has never caught on
+    even for a second and whilst she is constantly annoyed by his inability to be
+    mature she knows he cannot be replaced and has develop and slight lenience 
+    with him though others know not to test her bounds.""",
 
     "inventory": []
 
@@ -26,7 +40,18 @@ Jenifer = {
     "name": "Jenifer",
 
     "description":
-    "Jenifer is Samantha's secretary, she is useless at her job",
+    """Jenifer landed herself a position as Samantha's secretary by accident. Unknown to
+    herself and others, her name and documents weren't matching however becasue of a 
+    mistake in paperwork she was pulled aside one afternoon from her old job and was 
+    offered her current one. She said yes without hesitating and the organisation felt 
+    they had bagged a bargin. Little did they know she was utterly useless due to her
+    obsession with arbitrary things. Whilst she spends most of her time playing games 
+    and dealing with her makeup the organisation are reluctant to replace her. She had 
+    already signed the Official Secrecy Act and her background check showed clean. If 
+    anything the organisation would have to repeat the lengthy recruitment process again.
+    To make up for the lack of her work the organisation mostly dumps her work on Bob's
+    desk though the organisation haven't told him that his increased workload and non-
+    existant change in sallary is purely Jenifer's doing.""",
 
     "inventory": []
 }
@@ -36,7 +61,17 @@ Debra = {
     "name": "Debra",
 
     "description":
-    "Debra is head of Human Resources ",
+    """Debra has worked at the organisation since before Samanta took over the as head.
+    She was assigned to be reposible for Personell. Some would argue that she is in 
+    charge of Human Resources however an organisation such as this one is so different
+    that they decided not to use the traditional name and proivide her with her own 
+    title. Whilst being very good at her job she is often mistaken for being lazy. Truth
+    be told, she deals with work quicker than the organisation produces it. No one knows
+    how she achieves this but she is frequently seen endulging herself with snacks in 
+    the break room. Due to her excessive ammount of free time and Jenifer's inability to
+    work, the two share a friendship some would call extremely close. Their abundance of 
+    free time leads to hours of chatter and the two have grown close in the time that 
+    they have spent together""",
 
     "inventory": []
 
@@ -46,7 +81,20 @@ Bob = {
     "name": "Bob",
 
     "description":
-    "Bob is the Document manager",
+    """In every organisation, company or business there is always an employee who doesn't
+    get enough recognition. In this case Bob fills this position. Whilst almost always 
+    overlooked, Bob's job is to know the whereabouts of every document and its contents. 
+    The organisations choice to use paper documents stems from the statement that a 
+    computer can be hacked but a physical document has to be stolen. As a result Bob's job
+    was created so that normal file handelers can proceed with the necessary stapling etc
+    but only one person would be in charge of organisation and storage. His job was given
+    to him based on his capacity to memorise information. On the surface you'd assume he 
+    was an intellect but his memorisation patterns allow him to recall information from 
+    months beforehand at a seconds notice. He is extremely unappreciated by his fellow 
+    co-workers as they always claim a computer system could do his job better but he 
+    never lets it get to him. In his mind he only needs to be appreciated by Samantha 
+    because that's where his paychecks come from, and as far as he is concerned, Samantha
+    really does appreciate him.""",
 
     "inventory": []
 
@@ -57,9 +105,27 @@ Stephen = {
     "name": "Stephen",
 
     "description":
-    "Stephen is the technician for the agency",
+    """With organisations of high status it is not uncommon for a technology research 
+    department to be present. The high funding and necessity for technology provides 
+    Stephen with a well paid and secure job. Dr Stephen Arding was arrested multiple 
+    times for accounts of unregulated experimentation and forging of scientific 
+    documents. His defence was that the power of information should not be put in the 
+    hands of corrupted politicians. Dr Arding was given an opportunity out of his 4 
+    life sentences and he took it eagerly. The organisation was looking for a research
+    scientist who's absence wouldn't be noticed. Therefore an operation was held in 
+    which Alexa and Bruce broke into the high level security jail and retrieved the Dr
+    whilst planting a replacement identical body of Dr Arding with his DNA all over 
+    him. The official autopsy eventually reveled that the body wasn't Dr Arding 
+    however he had been missing for long enough that the government believed the 
+    investigation trails would have gone cold. Despite his reputation the government
+    closed the investigation because they believed he woulnd't have access to high 
+    tech equiptment without them realising. Little did they know he was hired right 
+    under thier noses and was eventually working for them. During his time at the 
+    organistion he has gained a decent reputation however in order to keep his true
+    identity secret his surname and status was never revealed to the other operatives.
+    To them he is just Stephen much like how Q is to 007.""",
 
-    "inventory": []
+    "inventory": [item_usb]
 
 }
 
@@ -68,7 +134,26 @@ Alexa = {
     "name": "Alexa",
 
     "description":
-    "Alexa is a spy, she is very rude and harsh",
+    """Alexa's origins are unknown to the other's within the organisation. As far as 
+    they are concerned she was a high performing field operative for the marines but 
+    was dishonourably discharged due to a direct violation of orders from her 
+    commanding officer. Her true backstory is only known by Samantha who was told when
+    she took over the operation. The passing of laws against human experimentation led
+    to the conviction of many "backstreet" Dr's trying to experiment on those who were
+    poor enough to donate their bodies in hopes of surviving experimentation and 
+    claiming a reward the Dr's so boastfully advertised. Alexa was abducted from her 
+    high school in Latvia at the age of 14 but managed to escape from the facility
+    she was kept at after an experiment on her nervous system gave her increased 
+    reflexes and cognitive ability. She remained wandering the streets of Riga until 
+    the age of 16 where she was brought into an orphanage on the assumption she had 
+    lost her parents. When the orphanage learned of her high cognitive function they 
+    alerted the Latvian government, however the transport of her was intercepted by 
+    the organisation and she was re-conditioned to become a field operative for them.
+    Her re-conditioning allowed her to re-adjust to her life and while she is always
+    second to Bruce she keeps trying to push her limits in hopes that she will surpass 
+    him. Her family and home are but distant memories that barely exist and now all she 
+    focuses on is her work and what she does to protect others so that they may not
+    experience the horrors she had faced as a young teenager.""",
 
     "inventory": []
 
