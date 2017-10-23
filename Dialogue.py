0b81de7b95ad0a4fbb49ac2b7ab0cc144c5f8539
Spooky-Jiangshi <owen.choy99@googlemail.com>
2017-10-23 12:54:43 +0100

    All dialogue up to the split in storyline in stage 4

---
+++ Dialogue.py
@@ -0,0 +1,455 @@
+stage_1 = {
+	
+	"Jenifer" : 
+	"""Jenifer : Hey Bruce, Samantha wants to see you.
+
+	Bruce : Oh wow what a suprise, my boss wants to see
+	me. It's not like I work for her or anything. What
+	does the old hag want?
+
+	Jenifer : I don't know, I'm not exactly psychic. 
+	Besides she won't tell me anyway. She doesn't tell
+	me anything that goes on in this place.
+
+	Bruce : Well maybe if you did some work from time
+	to time she might tell you things to do with, you 
+	know, work.
+
+	Jenifer : I'm still getting paid either way so I'll
+	just stick with doing nothing whilst you and the 
+	others can work your asses off.
+
+	Bruce : Well thanks for the motivation. I'll go 
+	talk to Samantha and try not to think of people or
+	inanimate objects that could replace you.
+
+	Jenifer : Rude""",
+
+
+
+	"Leaving" :
+	"""Jenifer : Where are you going? I told you 
+	Samantha wants to speak to you! Do you not have 
+	ears?
+
+	Bruce : I have ears, I just have selective hearing.
+
+	Jenifer : Go speak to her will you? She'll keep 
+	pestering me if I don't get you to speak to her.
+
+	Bruce : Gosh I guess this is an important thing
+	No wonder shes's asking for...
+
+	Jenifer : Don't say it!
+
+	Bruce : The best spy in the world!
+
+	Jenifer : You sound like a hyperactive five year
+	old when you say that.
+
+	Bruce : Does it look like I care? It's a cool 
+	title. I'll go speak to Samantha because 
+	obviously she needs me."""
+}
+
+stage_2 = {
+	
+	"SamanthaStart" : 
+	"""Samantha : Bruce! What time do you call this?
+
+	Bruce : Time you got a watch.
+
+	Samatha : I call it two hours too late. Anyway 
+	following your most recent field report all 
+	agents except you and Alexa are in the field. 
+	We can't assign anything until new intel is 
+	acquired. 
+
+	Bruce : So bassically you're telling me I have 
+	the day off.
+
+	Samantha : No I'm not giving you a day off. 
+	Debra and Bob have both reported that their
+	computers have been acting up recently and I 
+	need Stephen to take a look at the mainframe.
+
+	Bruce : My job has been downgraded from spy 
+	to messenger?
+
+	Samantha : Look, make yourself useful and 
+	help Stephen investigate what's going on with
+	the mainframe and see if you can fix it.
+
+	Bruce : Oh, so my job has been downgraded to 
+	IT help technician instead, noted.
+
+	Samatha : I'm already paying you enough so go do
+	something productive!
+
+	Bruce : OK ok. Got it""",
+
+
+
+	"SamanthaRepeat" : 
+	"""Samantha : Well don't stand there staring at
+	me! Go and help Stephen before I decide you need
+	permenant days off""",
+
+
+
+	"Jenifer" :
+	"""Jenifer : So what did she want?
+
+	Bruce : She wants me to help Stephen look at the
+	mainframe. 
+
+	Jenifer : Riiiiight, so she's given you a task
+	just to keep you out of trouble.
+
+	Bruce : ...""",
+
+
+
+	"JeniferRepeat" : 
+	"""Jenifer : Don't you have some important thing 
+	with Stephen to do?""",
+
+
+
+	"Debra" :
+	"""Bruce : Hey Debra, do you mind passing me some
+	biscuits?
+
+	Debra : Only if you don't mind replacing them...
+
+	Bruce : Oh come on! There's only one left? You're
+	seriously defeating the point of the rules.
+
+	Debra : I don't have to replace them if I don't 
+	finish them.
+
+	Bruce : Well you might as well replace them 
+	because you're the one whos always eating them.""",
+
+
+
+	"DebraRepeat" : 
+	"""Debra : So you gunna buy me more biscuits or 
+	what?
+
+	Bruce : No thanks.
+
+	Debra : Suit yourself.""",
+
+
+
+	"Bob" :
+	"""Bob : Oh hi Bruce, what brings you down here?
+	Do you need some paperwork?
+
+	Bruce : No Bob. I'll be honest, Samantha has sent 
+	me down to help Stephen look at the mainframe or 
+	something. 
+
+	Bob : Oh ok. Sounds interesting.
+
+	Bruce : Interesting is being out in the field
+	on a do or die mission with lives on the line.
+	This is just IT support.
+
+	Bob : Could be worse. You could have my job...
+
+	Bruce : Very true. I'll leave you to get back to
+	your filing and other important things.
+
+	Bob : *sighs* Thanks...""",
+
+
+
+	"BobRepeat" : 
+	"""Bob : You done helping Stephen yet?
+
+	Bruce : Uuuuuh, no. I'll get back to that...""",
+
+
+
+	"Alexa" :
+	"""Bruce : So how come you're not out in the field?
+
+	Alexa : My last mission gained high heat. I've been
+	told to lay low until my profile has lost it.
+
+	Bruce : Wah Wah someone might recognise me in the 
+	field. Got anymore lame excuses?
+
+	Alexa : You're so immature. No wonder Samantha is
+	hesitant to put you in the field.
+
+	Bruce : I'm not going to argue with the maturity
+	but I'll have you know I'm on an important internal
+	assignment that the organisation depends upon.
+
+	Alexa : ... Really?
+
+	Bruce : Yes. I'm helping Stephen fix the mainframe
+
+	Alexa : Oh please, Stephen could fix it without you.
+	Samantha is just making him babysit you.
+
+	Bruce : I don't need babysitting!
+
+	Alexa : Whatever you say hun. Whatever you say...""",
+
+
+
+	"AlexaRepeat" : 
+	"""Alexa : Excuse me sir can you help me with my 
+	phone?
+
+	Bruce : I'm not an IT technician Alexa. Quit it.""",
+}
+
+stage_3 = {
+
+	"StephenStart" : 
+	"""Bruce : Hey Stephen, Samantha told me to come
+	help you.
+
+	Stephen : Really!? Great! Come over here and attach
+	these to your nipples.
+
+	Bruce : NO STEPHEN! She said you have to take a
+	look at the mainframe because its faulty or 
+	something and I have to help.
+
+	Stephen : Can't you see I'm a little busy at the 
+	moment?
+
+	Bruce : Yeah I see you're busy electrocuting your
+	nipples "for science".
+
+	Stephen : Well I'm busy so I can't help right now
+
+	Bruce : Aw come on. Samantha's going to go crazy
+	if we don't fix this. She was really tense when I
+	spoke to her.
+
+	Stephen : Well, I do have something I could do. I 
+	created an executable and stored it on a USB for 
+	a situation like this.
+
+	Bruce : A situation when the mainframe acts up?
+
+	Stephen : Yes. The USB should fix the problem. 
+	The executable is prepared for pretty much every
+	possible problem the mainframe could be having.
+	All you need to do is log on and run it. But 
+	don't use your login incase it doesn't work and
+	it screws it up. That way the logs don't show it
+	was you.
+
+	Bruce : So... can I have your login details?
+
+	Stephen : Why would I give you mine? Just go 
+	find someone else's details and bring back 
+	whatever the mainframe prints out after the 
+	executable fixes it.
+
+	Bruce : Got it.""",
+
+
+
+	"StephenRepeat" : 
+	"""Stephen : Not now Bruce, just bring me back the
+	printed report when you get it.""",
+
+
+
+	"AlexaStart" : 
+	"""Bruce : Do you know where the key to the 
+	mainframe is kept? 
+
+	Alexa : Why would I know? Why didn't you ask 
+	Stephen?
+
+	Bruce : I tried but he was too busy fiddling with 
+	his nipples. 
+
+	Alexa : Oh God too much information.
+
+	Bruce : Anyway I need the dam key to get into the
+	mainframe room. You seen it anywhere?
+
+	Alexa : Now that I think about it, I think I saw
+	something like a key at the bottom of the toilet 
+	bowl when I went earlier.
+
+	Bruce : You've been eating keys? I didn't know 
+	that was necessary for a healthy diet.
+
+	Alexa : No I didn't eat the key. I just think I 
+	saw it there. Maybe someone dropped it.
+
+	Bruce : Or maybe someone is supplementing their 
+	nutritional intake with some keys. 
+
+	Alexa : Well that's all I got on the where abouts
+	of the key. Don't say I didn't help.
+
+	Bruce : It wasn't much help. You've just told me 
+	to go fishing in the toilet.
+
+	Alexa : So go do it. Maybe you'll make some 
+	friends with the turds.
+
+	Bruce : ...""",
+
+
+
+	"AlexaRepeat" : 
+	"""Alexa : How's turd fishing? Made any friends?""",
+
+
+
+	"AlexaFound" : 
+	"""Bruce : Alexa you were right! The key was in 
+	the toilet bowl. I almost hurled trying to fish it
+	out
+
+	Alexa : Yeah I did say. 
+
+	Bruce : Well thanks but I do feel quite queesy now. 
+	What idiot would drop this key in the toilet?
+
+	Alexa : What idiot would fish something out of the
+	toilet with his hands instead of using some cooking
+	tongs from the kitchen?
+
+	Bruce : That help would have been better earlier.
+
+	Alexa : I thought you'd be smarter.
+
+	Bruce : ...""",
+
+
+
+	"BobStart" : 
+	"""Bob : Hey Bruce sorry, I'm a little busy right
+	now. I needed the space to lay out the documents so
+	I'm gunna be in here instead of at my desk for a 
+	while. Do you mind if I get on with this?
+
+	Bruce : No problem.""",
+
+
+
+	"BobRepeat" : 
+	"""Bob : You done helping Stephen yet?
+
+	Bruce : Uuuuuh, no. I'll get back to that...""",
+
+
+
+	"DebraRepeat" : 
+	"""Debra : So you gunna buy me more biscuits or 
+	what?
+
+	Bruce : No thanks.
+
+	Debra : Suit yourself.""",
+
+
+
+	"JeniferRepeat" : 
+	"""Jenifer : Don't you have some important thing 
+	with Stephen to do?""",
+
+
+
+	"SamanthaRepeat" : 
+	"""Samantha : Well don't stand there staring at
+	me! Go and help Stephen before I decide you need
+	permenant days off""",
+}
+
+stage_3_2 = {
+
+	"JeniferStart" : 
+	"""Bruce : The printer is out of ink.
+
+	Jenifer : Um. Ok?
+
+	Bruce : ... I said, the printer is out of ink.
+
+	Jenifer : So what do you expect me to do about it?
+
+	Bruce : Uh I expected you to get up and replace 
+	the ink cartridge in the printer.
+
+	Jenifer : That's not in my job description.
+
+	Bruce : Neither is sitting at your desk and doing
+	nothing all day.
+
+	Jenifer : Well I'm not doing it so you do it.
+
+	Bruce : Fine. I'll do it.""",
+
+
+
+	"JeniferRepeat" : 
+	"""Jenifer : Don't you have some important thing 
+	with Stephen to do?""",
+}
+
+stage_4Coffee = {
+
+	"StephenStart" : 
+	"""Stephen : Ah Bruce. Thank you for the documents.
+	I'll have a look at those later. Right now I'm in 
+	the middle of an experiment.
+
+	Bruce : I'd kind of like you to take a look at them
+	now. Otherwise Samantha will be on our asses about
+	it.
+
+	Stephen : Don't worry the USB will have done it's 
+	job and everything should be fine.
+
+	Bruce : Can you have a look at it now please. I 
+	want to know if I can spend the rest of the day in
+	the break room.
+
+	Stephen : Fine I'll have a look at it. Pass it here.
+	Hmmm all seems fine. Wait. There's an anonymous 
+	entry in the vault records under "technology". 
+
+	Bruce : Aren't all entries logged with a name and 
+	time?
+
+	Stephen : Yes. All loggs have to conform to a 
+	standard I set. This log is impossible. Bruce, you
+	need to go and fetch me that piece of unknown tech.
+
+	Bruce : Nope. No. Can. Do. I was told to help fix
+	the mainframe which is now fixed so I can now do 
+	nothing and get paid.
+
+	Stephen : Do you think Samantha would be pleased 
+	to know what you've been using the organisation 
+	database to do. I have your search history.
+
+	Bruce : I'll get you that tech as if my life 
+	depends on it.
+
+	Stephen : You better do. Because judging by 
+	the categories you search up, your life does
+	depend on it. Also, try not to drag other people
+	into this. It may seem insignificant but it 
+	could be rather serious.""",
+
+
+
+	"StephenRepeat" : 
+	"""Stephen : Have you got the tech yet?""",
+}
