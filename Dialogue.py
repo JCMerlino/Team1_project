eebf849d8328c4fa227f7664cfaca8711664ec5a
Owen <owen.choy99@googlemail.com>
2017-10-23 20:01:55 +0100

    Finished Dialogue

--- Dialogue.py
+++ Dialogue.py
@@ -206,7 +206,7 @@ stage_2 = {
 	"""Alexa : Excuse me sir can you help me with my 
 	phone?
 
-	Bruce : I'm not an IT technician Alexa. Quit it.""",
+	Bruce : I'm not an IT technician Alexa. Quit it."""
 }
 
 stage_3 = {
@@ -306,7 +306,7 @@ stage_3 = {
 
 
 
-	"AlexaRepeat" : 
+	"AlexaRepeat1" : 
 	"""Alexa : How's turd fishing? Made any friends?""",
 
 
@@ -330,6 +330,12 @@ stage_3 = {
 	Alexa : I thought you'd be smarter.
 
 	Bruce : ...""",
+
+
+
+	"AlexaRepeat2" : 
+	"""Alexa : Quit procrastinating by talking to me
+	and go finish your business with Stephen.""",
 
 
 
@@ -369,7 +375,7 @@ stage_3 = {
 	"SamanthaRepeat" : 
 	"""Samantha : Well don't stand there staring at
 	me! Go and help Stephen before I decide you need
-	permenant days off""",
+	permenant days off"""
 }
 
 stage_3_2 = {
@@ -399,7 +405,7 @@ stage_3_2 = {
 
 	"JeniferRepeat" : 
 	"""Jenifer : Don't you have some important thing 
-	with Stephen to do?""",
+	with Stephen to do?"""
 }
 
 stage_4Coffee = {
@@ -427,7 +433,7 @@ stage_4Coffee = {
 	Bruce : Aren't all entries logged with a name and 
 	time?
 
-	Stephen : Yes. All loggs have to conform to a 
+	Stephen : Yes. All logs have to conform to a 
 	standard I set. This log is impossible. Bruce, you
 	need to go and fetch me that piece of unknown tech.
 
@@ -437,7 +443,7 @@ stage_4Coffee = {
 
 	Stephen : Do you think Samantha would be pleased 
 	to know what you've been using the organisation 
-	database to do. I have your search history.
+	resources to do. I have your search history.
 
 	Bruce : I'll get you that tech as if my life 
 	depends on it.
@@ -452,4 +458,768 @@ stage_4Coffee = {
 
 	"StephenRepeat" : 
 	"""Stephen : Have you got the tech yet?""",
+
+
+
+	"JeniferRepeat" : 
+	"""Jenifer : Don't you have some important thing 
+	with Stephen to do?""",
+
+
+
+	"SamanthaStart" : 
+	"""Samantha : Bruce, an update on the situation 
+	with the mainframe?
+
+	Bruce : You'll be pleased to know that Stephen and
+	I have investigated the problem and we are sorting
+	it out right now.
+
+	Samantha : You seem to be taking something seriously
+	for once. I'm pleased to see that. Anything else 
+	you need to report?
+
+	Bruce : I fished the keys to the mainframe and 
+	vault out of the toilet. Does that count?
+
+	Samantha : You can't even stay serious for more 
+	than five minutes?? Just get out of my office and
+	get on with the job!
+
+	Bruce : ...""",
+
+
+
+	"SamanthaRepeat" : 
+	"""Samantha : Go get the job finished! And take it
+	seriously or I'll cut your pay!""",
+
+
+
+	"DebraStart" :
+	"""Bruce : Do you ever leave the break room?
+
+	Debra : Not if I can help it.
+
+	Bruce : You are unbelievable. Do you actually work?
+
+	Debra : Yes I do. The big pile of paperwork that Bob
+	has is thanks to me completing all my paperwork so 
+	quickly that he has to file it away.
+
+	Bruce : I almost want to believe its true.
+
+	Debra : Don't you have something to do?
+
+	Bruce : Yeah turns out I have work to do. Something
+	you and Jenifer don't have the pleasure of doing.""",
+
+
+
+	"DebraRepeat" : 
+	"""Debra : So you finished your work yet?
+
+	Bruce : Nope. It's long and difficult and above your
+	paygrade.""",
+
+
+
+	"BobStart" : 
+	"""Bruce : So someone left the vault password at the
+	mainframe. Do you know who last used the mainframe?
+
+	Bob : I don't really know who uses the mainframe 
+	sorry.
+
+	Bruce : I thought you kept all the logs in the 
+	organisation?
+
+	Bob : Yeah but not electronic logs. You better ask
+	Stephen for those.
+
+	Bruce : Oh yeah... I knew that.""",
+
+
+
+	"BobRepeat" : 
+	"""Bob : Have you asked Stephen about those logs yet?
+
+	Bruce : He's quite busy right now. Besides I have 
+	something else to do.""",
+
+
+
+	"AlexaStart" : 
+	"""Bruce : When was the last time you put something
+	in the vault?
+
+	Alexa : A long time ago. Why'd you ask?
+
+	Bruce : Oh nothing. It's just I overheard something
+	about the vault and was wondering if people knew 
+	anything about it.
+
+	Alexa : Give me a heads up if you find out anything
+	else about the vault.
+
+	Bruce : Will do.""",
+
+
+
+	"AlexaRepeat" : 
+	"""Alexa : Any info on the vault?
+
+	Bruce : Nope. I'll get back to you when I find 
+	something."""
+}
+
+stage_5Coffee = {
+
+	"StephenStart" : 
+	"""Stephen : Now this is interesting... 
+
+	Bruce : So you know what it is?
+
+	Stephen : Yes I do and I know what to do with it.
+	I need a power plug, a heat plate and some mugs.
+
+	Bruce : Why do you need those?
+
+	Stephen : I'll explain later. Just make sure that
+	the mugs are ceramic. It's important.
+
+	Bruce : Can I at least know what you're going to 
+	do with those items?
+
+	Stephen : I'm going to fix one of the machines 
+	that has been broken for a while.
+
+	Bruce : Ok that sounds reasonable but why do I 
+	have to go and fetch them? 
+
+	Stephen : Because you'll get to use the machine
+	when you need to.
+
+	Bruce : Fair enough. I'll get you the items.""",
+
+
+
+	"StephenRepeat" : 
+	"""Stephen : Have you got all the items yet?
+
+	Bruce : No but I'll keep searching.
+
+	Stephen : Good. Come back as soon as you have
+	them.""",
+
+
+
+	"SamanthaStart" : 
+	"""Samantha : What is it now?
+
+	Bruce : Just popping in to say that the fixing
+	of the mainframe is going very well.
+
+	Samantha : Ok. Stop wasting my time and get 
+	back to it.""",
+
+
+
+	"SamanthaRepeat" : 
+	"""Samantha : Stop wasting my time and get back 
+	to fixing the mainframe.""",
+
+
+
+	"JeniferStart" : 
+	"""Bruce : Do you have any idea where I'd find 
+	a spare power plug?
+
+	Jenifer : Well if you were smart you'd take one
+	from a computer whose owner does no work. That 
+	way it wouldnt be missed.
+
+	Bruce : In that case can I have your power plug?
+
+	Jenifer : Of course not. I'm watching Netflix. 
+	Don't worry. If you finish work quickly I might
+	invite you to chill with me.
+
+	Bruce : I'll consider it. I guess I'll have to
+	find someone else who does no work.""",
+
+
+
+	"JeniferRepeat" : 
+	"""Jenifer : Hurry and finish your work so we 
+	can chill together.""",
+
+
+
+	"DebraStart" : 
+	"""Debra : You've visited this room so many 
+	times I'm starting to think you don't do any
+	work.
+
+	Bruce : Excuse me. I've come to this room on 
+	work related terms.
+
+	Debra : Really?
+
+	Bruce : Yeah I came to take some mugs.
+
+	Debra : "Work related" he says.
+
+	Bruce : Yes. Stephen needs them for something
+	he's fixing.
+
+	Debra : Well whatever you do don't take my mug.
+
+	Bruce : Don't worry I won't.""",
+
+
+
+	"DebraRepeat" : 
+	"""Debra : You better not have taken my mug.""",
+
+
+
+	"BobStart" : 
+	"""Bob : Hi Bruce hows your day going?
+
+	Bruce : Well it's been pretty underwhelming so 
+	far. I've done all this stuff and I don't really
+	see much outcome for it.
+
+	Bob : Sometimes you have to wait till the end 
+	to see what you've really worked for.
+
+	Bruce : That's stupid. I should know what I'm 
+	working for from the start.
+
+	Bob : So you don't know what you've been 
+	working for today?
+
+	Bruce : Nope not a clue. I just know I have to 
+	collect some stuff for Stephen now.
+
+	Bob : Do you need help?
+
+	Bruce : I don't know where to get a heat plate
+	from.
+
+	Bob : Why don't you try storage?
+
+	Bruce : Yeah why didn't I think of that?""",
+
+
+
+	"BobRepeat" : 
+	"""Bob : You finished helping Stephen yet?
+
+	Bruce : Nope.""",
+
+
+
+	"AlexaStart" : 
+	"""Bruce : Hey Alexa do you who's mugs are
+	easiest to steal?
+
+	Alexa : ...??
+
+	Bruce : It's not a joke it's for Stephen.
+
+	Alexa : ...??
+
+	Bruce : Ok well I'm sure Stephen is just
+	borrowing them and they will be returned in
+	good condition.
+
+	Alexa : Try taking Debra's mugs. She'll 
+	probably be too busy eating to notice you're 
+	taking them.""",
+
+
+
+	"AlexaRepeat" : 
+	"""Alexa : Still being Stephen's errand boy?"""
+}
+
+stage_6Coffee = {
+
+	"StephenStart" : 
+	"""Stephen : Ah Bruce! Thanks for getting all
+	those. In honesty I felt quite bad for making you
+	go and fetch all these things after I told you to 
+	go but you'd already left so I thought I'd just 
+	let you continue.
+
+	Bruce : You lazy ass! I've gone around getting all
+	this stuff for you! What do I get in return?
+
+	Stephen : The finest cup of coffee you've ever had.
+
+	Bruce : Wait what?
+
+	Stephen : The parts. They're to repair one of the
+	best coffee machines in the world. The tech that you
+	got from the vault was a communicator that was 
+	perfect size for me to put in this coffee maker so 
+	that I can make coffee remotely from my phone.
+
+	Bruce : You said a machine.
+
+	Stephen : Coffee MACHINE aaaaand that about does it.
+	Good as new. Hand me a mug so we can test it out.
+
+	*Enters Samantha
+
+	Samantha : Good God what is the meaning of this!?
+
+	Bruce : Stephen can explain.
+
+	Stephen : Uh What?
+
+	Samantha : You two were taking so long on getting
+	back to me about the mainframe I came here to get
+	an update and I see you wasting your time with your
+	coffee machine! What have you been doing??
+
+	Stephen : Don't worry. The mainframe is fixed. I 
+	gave Bruce a USB stick with an executable that fixes
+	the mainframe by itself. It should have finished long
+	ago so I fixed a machine. Actually I need that USB,
+	can you go get it for me Bruce?
+
+	Samantha : Wait one moment! Bruce! Every time you
+	spoke to me you said you were fixing the mainframe!
+	You lied to me!
+
+	Bruce : In my defence I didn't know what I was doing.
+
+	Samantha : Lousy defence. Go fetch Stephen's USB.
+
+	*Samantha storms out of the lab""",
+
+
+
+	"StephenRepeat" : 
+	"""Stephen : Have you got my USB?""",
+
+
+
+	"SamanthaRepeat" : 
+	"""Samantha : Don't talk to me you've wasted enough
+	time as it is.""",
+
+
+
+	"JeniferRepeat" : 
+	"""Jenifer : Sorry Bruce netflix mood has passed.
+	Maybe next time...""",
+
+
+
+	"DebraRepeat" : 
+	"""Debra : So what did Stephen need all those parts
+	for?
+
+	Bruce : He fixed some fancy coffee machine.
+
+	Debra : Really? 
+
+	Bruce : Yeah I kinda spend my whole day doing 
+	pointless things. 
+
+	Debra : Yeah you have.""",
+
+
+
+	"DebraRepeat" : 
+	"""Debra : What do I have to do to get you to run
+	errands for me?
+
+	Bruce : I won't do errands for you.
+
+	Debra : Aww please?
+
+	Bruce : No.""",
+
+
+
+	"BobRepeat" : 
+	"""Bob : How are you still doing errands for Stephen?
+
+	Bruce : I don't know... My job isn't supposed to be
+	this...""",
+
+
+
+	"AlexaRepeat" : 
+	"""Alexa : You look exhausted. You haven't even done
+	anything today. At least you get paid to do nothing.
+
+	Bruce : Yeah I guess..."""
+}
+
+stage_7Coffee = {
+
+	"StephenStart" : 
+	"""Stephen : Thank you Bruce. You've been really 
+	helpful today. I haven't even left my seat.
+
+	Bruce : Yeah and I've been up and around everywhere.
+
+	Stephen : Well anyway have a cup of coffee.
+
+	Bruce : It better be good.
+
+	Stephen : I think you'll like it.
+
+	Bruce : So, that piece of tech you put in there.
+	Why was it in the vault in the first place?
+
+	Stephen : No idea, but it was a communicator so 
+	it was probably transmitting information. Possibly
+	from a double agent or something.
+
+	Bruce : Shouldn't we alert Samantha or Debra?
+
+	Stephen : Probably, but Samantha is really not going
+	to listen to use right now and Debra will have left by
+	now. 
+
+	Bruce : So we just assume things will be ok?
+
+	Stephen : Yeah why not? I reconfigured the communicator
+	so now it only responds and communicates to my phone.
+	We have stopped an information leak whilst fixing my 
+	machine. I think that's worth the effort you went through
+	today.
+
+	Bruce : When you put it that way, yeah I guess it was 
+	worth the effort.
+
+	Stephen : Well now that that's that have some coffee.
+
+	Bruce : Sure thing.
+
+	Stephen : Now about your internet usage. I think you 
+	need to see someone about your pornography addiction.
+	Not necessarily the frequency but the content...
+
+	Bruce : WHAT DO YOU KNOW?
+
+	Stephen : Too much...
+
+	GAME END - INSERT CREDITS HERE?"""
+}
+
+stage_4Agent = {
+
+	"DebraStart" : 
+	"""Bruce : Hi Debra I have something you might want 
+	to take a look at.
+
+	Debra : You better not mean your manhood.
+
+	Bruce : Uh what? Anyway these logs that printed after
+	Stephen's fix worked look wrong. There shouldn't be an
+	anonymous log. I think someone is up to something.
+
+	Debra : Hmm it would appear so... We need to have a 
+	check around and see if there are any suspicous items 
+	around the place. And by we I mean you because I need
+	to stay available to do work in case more comes.
+
+	Bruce : You're making me go round and find items? 
+
+	Debra : You're the one with nothing to do at the 
+	moment so you should do it.
+
+	Bruce : What if something happens. What am i supposed 
+	to do?
+
+	Debra : Wow you must be a terrible field agent if 
+	you're asking me that question. You figure out what to
+	do. I'll wait for you to return with some evidence.""",
+
+
+
+	"DebraRepeat" : 
+	"""Debra : Chop Chop! We, I mean you need to find 
+	suspicious items!""",
+
+
+
+	"StephenRepeat" : 
+	"""Stephen : Thanks for putting the USB in the 
+	mainframe for me. Don't worry about the reports they
+	are boring to go through anyway.""",
+
+
+
+	"JeniferRepeat" : 
+	"""Jenifer : Bruce I think Samantha said she wanted
+	an update on that mainframe thingy.""",
+
+
+
+	"SamanthaStart" : 
+	"""Samantha : How is the mainframe?
+
+	Bruce : It looks like Stephen fixed it.
+
+	Samantha : Good. Stay out of trouble for the rest
+	of the day. Go help someone or something I don't have
+	anything unimportant for you to screw up.
+
+	Bruce : Ouch way to hurt a man's ego.""",
+
+
+
+	"SamanthaRepeat" : 
+	"""Samantha : I told you to go help someone else. I'm 
+	really busy at the moment.""",
+
+
+
+	"BobRepeat" : 
+	"""Bob : You finished helping Stephen yet?
+
+	Bruce : Yeah but I'm helping Debra with something now.
+
+	Bob : Oh ok don't let me stop you then.""",
+
+
+
+	"AlexaStart" : 
+	"""Bruce : Alexa have you seen anything suspicious lately?
+
+	Alexa : Your face.
+
+	Bruce : Not funny. Seriously are there any things out of
+	place?
+
+	Alexa : Not really. I don't think anything is out of the 
+	oridnary.
+
+	Bruce : Ok well keep an eye out will you?
+
+	Alexa : Fine I'll keep an eye out. Though...
+
+	Bruce : What?
+
+	Alexa : How long has that camera been there?
+
+	Bruce : I have no idea. I didn't notice it before.
+
+	Alexa : Intersting...""",
+
+
+
+	"AlexaRepeat" : 
+	"""Alexa : Sorry Bruce I haven't found anything suspicious looking."""
+}
+
+stage_5Agent = {
+
+	"DebraStart" : 
+	"""Debra : Well that's it. Someone must be leaking information to 
+	external people. It's too obvious. We have to be careful selecting
+	who to bring to Samantha. From what we know it has to be someone 
+	who has enough free time to put these items in their places and 
+	they have to have access to Samantha's office. 
+
+	Bruce : That's not much to go on.
+
+	Debra : You're a smart field agent. Figure it out. I'll keep these 
+	evidence items safe. Go find the double agent.""",
+
+
+
+	"StephenRepeat" : 
+	"""Stephen : Thanks for helping out earlier. I'm kinda doing some
+	important experiments and I need to focus. Sorry I can't chat.""",
+
+
+
+	"JeniferRepeat" : 
+	"""Jenifer : Are you still doing errands for people? Can you take
+	my trash out for me please?
+
+	Bruce : No I'm not a dustbin man.""",
+
+
+
+	"DebraRepeat" : 
+	"""Debra : Who have you deduced that the double agent is?
+
+	Bruce : I don't know yet.
+
+	Debra : Well hurry up. We have no idea what they could be doing 
+	whilst we are making our move."""
+
+
+
+	"SamanthaRepeat" : 
+	"""Samantha : I told you to go help someone else. I'm 
+	really busy at the moment.""",
+
+
+	"BobRepeat" : 
+	"""Bob : You finished helping Stephen yet?
+
+	Bruce : Yeah but I'm helping Debra with something now.
+
+	Bob : Oh ok don't let me stop you then.""",
+
+
+
+	"AlexaRepeat" : 
+	"""Alexa : Sorry Bruce I haven't found anything suspicious looking."""
+}
+
+stage_6Agent = {
+
+	"SamanthaStart" : 
+	"""Samantha : I can't believe it. No wonder the mixing up of the names
+	felt dodgy. You were planted here. By whom? I demand answers!
+
+	Jenifer : You won't find anything out.
+
+	Samantha : I hate to sound cliche but we have ways of making you talk.
+
+	Bruce : Are we going to torture her!? I miss this so much!
+
+	Samantha : Yes Bruce for once we're going with something you like and
+	I hope you're performance is good. 
+
+	Bruce : I always have good performance.
+
+	*click
+	*A bright flash filled the room acompanied by a deafening sound
+
+	Bruce : AARG What was that?
+
+	Samantha : What?
+
+	Bruce : It must have been a flash grenade!
+
+	Samantha : What?
+
+	Bruce : You can't hear?
+
+	Samantha : This is not the time for a can of beer now chase after her!
+
+	*Bruce chases Jenifer out of the reception and to the stairs
+
+	Bruce : She's going down the stairs!
+
+	Samantha : Shoot her!
+
+	Do you shoot her chest or her leg?"""
+}
+
+stage_7Agent = {
+
+	"SamanthaStart" : 
+	"""Samantha : Damm it Bruce you shot her in the chest. Look at her
+	she's going to bleed out!
+
+	Bruce : Yeah that is a lot of blood for such a small woman...
+
+	Samantha : Well thanks a lot. Now we can't interrogate her to find
+	out who put her here.
+
+	Bruce : Should we try and save her?
+
+	Samantha : Oh whats the point. She's practically unsavable now and
+	now she's gone we won't have an information leak. Just clean up 
+	blood and dump her body somewhere.
+
+	Bruce : Wow you are cold.
+
+	Samantha : I would have thought you'd have better field agent 
+	judgement and that you'd have shot her leg. You're on suspension for
+	a week to re-collect your thoughts. When you're back I want you sharp
+	and on edge for anything.
+
+	Bruce : Ok. 
+
+	*Alexa enters
+
+	Alexa : Bruce are you ok?
+
+	Bruce : Yeah I'm fine but I'm also on suspension.
+
+	Alexa : How come?
+
+	Bruce : For shooting a double agent.
+
+	Alexa : I just don't get Samantha sometimes...
+
+	GAME END - INSERT CREDITS HERE?"""
+}
+
+stage_8Agent = {
+
+	"SamanthaStart" : 
+	"""Samantha : Good shot Bruce!
+
+	Bruce : Thanks.
+
+	Samantha : Perfet. The bullet hasnt gone through the leg because it
+	got caught on the shin bone. I'll see to it personally that Jenifer 
+	spills the secrets. Maybe a knife pressing into this open wound would 
+	persuade her to talk. 
+
+	Bruce : Uhh since when have you been into this sort of thing?
+
+	Samantha : Oh I was very good at interogating from when I used to be 
+	a field agent too. 
+
+	Bruce : Shouldn't you check her in case she has another grenade?
+
+	Samantha : If she does she still won't be able to flee and that
+	definately means she won't use a lethal one if she has one.
+
+	Bruce : Ok so all is under control.
+
+	Samantha : Yes. Thanks to you we can find out who sent her and we can
+	give them a little piece of our mind. Get something to clean up the 
+	blood stains.
+
+	Bruce : So I guess something interesting did eventually happen today.
+	Shame I'm still doing errands for people.
+
+	GAME END - INSERT CREDITS HERE?"""
+}
+
+stage_9Agent = {
+
+	"SamanthaStart" : 
+	"""Samantha : This is outrageous Bruce!
+
+	Bruce : I can explain!
+
+	Samantha : You've wasted your time, you've wasted my time and you've
+	wasted other people's time!
+
+	Bruce : But there is an explanation for all this!
+
+	Samantha : An explanation for you going all over taking things and
+	harrassing co-workers?
+
+	Burce : Um... Yes?
+
+	Samantha : That just does it. You've really shown me that you can't be
+	trusted. Even here! I'm putting you under Suspension for wasting time
+	and resources. Report back here in a week. 
+
+	Bruce : Come on you haven't even given me a chance to explain myself.
+
+	Samantha : You don't have to! OUT!
+
+	GAME END - INSERT CREDITS HERE?"""
 }
