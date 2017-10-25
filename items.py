678923a464d0faab00c1ecdfb3c9961c0ed77ae4
Spooky-Jiangshi <owen.choy99@googlemail.com>
2017-10-25 11:24:49 +0100

    changed descriptions

--- items.py
+++ items.py
@@ -1,4 +1,4 @@
-from ascii_art import *
+from ascii_art import * 
 item_usb = {
     "id": "usb",
 
@@ -8,9 +8,9 @@ item_usb = {
 
     "description":
     """Stephen gave me this USB with his so called "Mainframe Fix" on it.
-The last time he handed me one of these he left extremely illegal pornography
-on it that really made me question his functioning capacity as a human being.
-He told me to plug this into the mainframe and log on. Of course he didn't give
+The last time he handed me one of these he left multiple viruses on it that 
+destroyed the computer I was working on and made me lose all my work. He
+told me to plug this into the mainframe and log on. Of course he didn't give
 me the login details so I'll have to go find out some. I'm sure it would have
 made my life a lot easier if he gave me details but I assume he wants me to
 use someone else's account so the blame goes on them if it screws up. Pretty
@@ -20,7 +20,7 @@ smart. I think I'll screw up Bob's account.""",
 }
 
 item_mainframe_key = {
-    "id": "mainframekey",
+    "id": "mainframe_key",
 
     "name": "mainframe key",
 
@@ -30,14 +30,13 @@ item_mainframe_key = {
     """Well we just won the worst security system award of the year.
 Seriously this is a physical key that unlocks the door to the mainframe.
 I mean, someone would still have to hack the mainframe but man why just a
-single key? Anyone could just pick that lock. If someone could hack into
-the system they could change everyone's search history... I might talk
-to Stephen about changing Bob's history.""",
-
-    "picture": key()
+single key? Anyone could just pick that lock. Wait... I knew it! I didn't
+search up furry porn when I was blackout drunk it was Stephen who picked
+the lock and changed the mainframe entries. Such a dick. Come to think of
+it now it is pretty funny. Maybe I should get him to do it on Bob."""
 }
 
-item_login = {
+item_login_details = {
     "id": "login",
 
     "name": "login details",
@@ -46,10 +45,8 @@ item_login = {
 
     "description":
     """Trust Bob to leave his login details on a scrap peice of paper. Too
-easy. Login:Bob Password:Bruceisacunt Considering how much I do for this
-service I think I deserve a little respect.""",
-
-    "picture": envelope_note()
+easy. Login:Bob Password:Brucesucks Considering how much I do for this
+service I think I deserve a little respect."""
 }
 
 item_ink = {
@@ -64,13 +61,11 @@ item_ink = {
 ink cardtridge when the printer runs out of ink but no let the field agent
 do it because why the hell not! Come to think of I should file a complaint
 because I seem to be the only one who does any work around here. Everyone
-else just stands around in their rooms not even moving.""",
-
-    "picture": ink()
+else just stands around in their rooms not even moving."""
 }
 
 item_vault_log = {
-    "id": "vaultlog",
+    "id": "vault_log",
 
     "name": "vault logs",
 
@@ -79,14 +74,13 @@ item_vault_log = {
     "description":
     """So printed out on this piece of paper is every item in the vault.
 Apparently Stephen can see if there is something fishy based on the times
-and entries of the information. Sounds like a very boring and tedious task.
-Thank god he didn't ask me to do it""",
-
-    "picture": envelope_note()
+and entries of the information. There is a weird entry here. It doesn't 
+look official. Maybe someone is up to something. Maybe I should give this
+to Debra instead..."""
 }
 
 item_vault_key = {
-    "id": "vaultkey",
+    "id": "vault_key",
 
     "name": "vault key",
 
@@ -97,13 +91,11 @@ item_vault_key = {
 the hell would anyone put it here? Or more worringly did someone eat it?
 Anyway maybe our security system should just not exist at all. At least
 this key only unlocks the outer door. Of course this means I also need
-the Vault Password as well.""",
-
-    "picture": key()
+the Vault Password as well."""
 }
 
 item_vault_pass = {
-    "id": "vaultpass",
+    "id": "vault_pass",
 
     "name": "vault password",
 
@@ -113,10 +105,8 @@ item_vault_pass = {
     """Well at least the actual password for the vault is decent. 10
 digit number which is like a lot of combinations. 1544973268 is the
 password I'm glad this password is decent. I put my trust in this password
-to guard my secret stash. I don't know why people think I have a problem
-when I clearly don't""",
-
-    "picture": envelope_note()
+to guard my secrets. I don't know why people think I have a problemwhen 
+I clearly don't"""
 }
 
 item_mugs = {
@@ -131,9 +121,7 @@ item_mugs = {
 needs these mugs. They aren't high tec or anything. Maybe he needs the
 ceramics. I hope Debra and Jenifer don't mind me taking their ones. No
 way I'm letting my mug being destroyed. I won it for best hairstyle at
-a primary school faire""",
-
-    "picture": mug()
+a primary school faire"""
 }
 
 item_power_lead = {
@@ -143,16 +131,17 @@ item_power_lead = {
 
     "mass": 0.8,
 
-    "description":
-    """You'd think that the science lab would have these just lying about but apparently not. I think Stephen is just being lazy and making me do his errands.
-The computer I took this from did say "DO NOT POWER OFF" but I think it was being melodramatic. I'm pretty sure things will be fine.""",
-
-    "picture": power_lead()
+    "description": 
+    """You'd think that the science lab would have these just lying about 
+    but apparently not. I think Stephen is just being lazy and making me 
+    do his errands. The computer I took this from did say "DO NOT POWER 
+    OFF" but I think it was being melodramatic. I'm pretty sure things 
+    will be fine."""
 
 }
 
 item_heat_plate = {
-    "id": "plate",
+    "id": "heat_plate",
 
     "name": "heat plate",
 
@@ -161,9 +150,7 @@ item_heat_plate = {
     "description":
     """Literally in the next room. Couldn't he just get off his lazy ass and
 go get it himself? This heat plate looks heavily used. But then again Stephen
-does like sloppy seconds""",
-
-    "picture": plate()
+probably won't mind"""
 }
 
 item_documents = {
@@ -178,9 +165,7 @@ item_documents = {
 Our physical reports, mental reports, health reports. Jeez these documents have
 everything. Anyone who gets hold of these will know everything about every field
 agent. I wish I'd found this sooner, these health and physical reports on female
-agents are rather interesting...""",
-
-    "picture": documents()
+agents are rather interesting..."""
 }
 
 item_gun = {
@@ -191,11 +176,10 @@ item_gun = {
     "mass": 1.5,
 
 
-    "description":
-    """My silenced pistol. Found tucked away in the generator room. I have to find
-out who's been using it. My gun can't cheat on me it's mine and mine alone.""",
-
-    "picture": gun()
+    "description": 
+    """This was found hidden in the meeting room. Considering there are no agents 
+    in the field currently ,no one should have access to a gun at all. Perhaps I 
+    should dust it for prints or whatever forensic people do. """
 
 }
 
@@ -207,15 +191,10 @@ item_camera = {
     "mass": 0.7,
 
     "description":
-    """They say profesional work conduct is essential for a safe and effective working
-environment but someone obviously put this here to catch people getting a little too
-close to each other. I wonder what sort of creep wants to record their co-workers
-doing the dirty. I better find out who put this camera in the meeting room. Actually
-maybe this camera was used to record important meetings by a mole or something. Either
+    """I better find out who put this camera in the meeting room. Actuallymaybe 
+    this camera was used to record important meetings by a mole or something. Either
 way I don't want them finding out what else goes on in the meeting room especially if
-it concerns me.""",
-
-    "picture": camera_recorder()
+involves me."""
 }
 
 item_recorder = {
@@ -225,27 +204,12 @@ item_recorder = {
 
     "mass": 0.5,
 
-    "description":
-    """This isn't Samanthas so this was obviously put in Samantha's room to eavesdrop
-on her conversations. I wouldn't be suprised if it was one of us who put it there
-because she makes shady deals with other organisations that really screw us over.
-Though this might be linked to the camera and about gathering important information.
-Wouldn't that be embarrassing... If the professional spies were being spied on. I
-should probably alert Debra about a possible double agent""",
+    "description": 
+    """This isn't Samanthas so this was obviously put in Samantha's room to eavesdrop 
+    on her conversations. I wouldn't be suprised if it was one of us who put it there 
+    because she makes shady deals with other organisations that really screw us over. 
+    Though this might be linked to the camera and about gathering important 
+    information. Wouldn't that be embarrassing... If the professional spies were being
+    spied on. I should probably alert Debra about a possible double agent"""
 
-    "picture": camera_recorder()
-}
-
-item_tech = {
-    "id": "tech",
-
-    "name": "Unknown Tech",
-
-    "mass": 1.5,
-
-    "description":
-    """This intersting thingy is just blinking constantly. I hope it's a bomb. That will put me out my mysery. I should
-probably give this to Stephen or show Debra.""",
-
-    "picture": tech()
 }
